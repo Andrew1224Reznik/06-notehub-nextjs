ad8231c8818393e0d92b1b57839536307348ba85
Andrew1224Reznik <reznik1224@gmail.com>
2026-02-13 23:45:05 +0200

    fix

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import css from './NoteDetails.client.module.css';
+
+import { useQuery } from '@tanstack/react-query';
+import { fetchNoteById } from '../../../lib/api';
+import { useParams } from 'next/navigation';
+import { Note } from '../../../types/note';
+
+interface Props {
+  noteId: string;
+}
+export default function NoteDetails({ noteId }: Props) {
+  const params = useParams<{ noteId: string }>();
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery<Note | null>({
+    queryKey: ['note', noteId],
+    queryFn: () => fetchNoteById(noteId),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading, please wait...</p>;
+  if (isError || !note) return <p>Something went wrong.</p>;
+
+  return (
+    <div className={css.container}>
+      <div className={css.item}>
+        <div className={css.header}>
+          <h2>{note.title}</h2>
+        </div>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>Created at: {note.createdAt}</p>
+      </div>
+    </div>
+  );
+}
